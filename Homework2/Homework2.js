8a647ebcc3baad87a8dae4e7d5357c0726ae46ce
dannypadilla <disavowed10@gmail.com>
2016-10-04 15:56:27 -0700

    finish part 3

--- Homework2/Homework2.js
+++ Homework2/Homework2.js
@@ -1,5 +1,5 @@
 // Homework #2
-
+"use strict";
 function rowHeights(rows) {
   return rows.map(function(row) {
     return row.reduce(function(max, cell) {
@@ -178,41 +178,65 @@ upper.push([new BorderedCell("Denny's"), new BorderedCell("$")]);
 console.log();
 console.log("Entire array");
 console.log(upper);
-console.log("Each Row");
+console.log("First Row");
 console.log(upper[0]);
-console.log("Each TextCell Object");
+console.log("First Text Cell Object");
 console.log(upper[0][0]);
-console.log("text property");
+console.log("First Text Cells Text");
 console.log(upper[0][0].text);
 
+/*
 function cellToUpperCase(dataArray, rowNum) {
     return dataArray[rowNum].map(function(x) {
 	return x.text[0].toUpperCase();
     });
+}
+*/
+
+// Get Row
+function getRow(dataArray, rowNumber) {
+    return dataArray[rowNumber];
 }
 
 console.log();
 console.log("ToUpperCase Call");
 //console.log(cellToUpperCase(upper, 1));
 
-console.log(drawTable(cellToUpperCase(upper, 1)));
+//console.log(drawTable(cellToUpperCase(upper, 1)));
 
 // Problem 5
 console.log();
 console.log("Problem #5:");
-var people = []; 
+//var people = [];
+
 var person = [
-    {"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"},
+    [{"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"},
     {"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"},
-    {"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"},
-    {"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"},
+    {"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"}],
+    [{"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"},
     {"name": "Philibert Haverbeke", "sex": "m", "born": 1907, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"},
-    {"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"},
-    {"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"},
+    {"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"}],
+    [{"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"},
     {"name": "Lieven de Causmaecker", "sex": "m", "born": 1696, "died": 1724, "father": "Carel de Causmaecker", "mother": "Joanna Claes"},
-    {"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"}
+    {"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"}]
 ];
 
-//theEnd
+var people = person.map(function(personRow) {
+    return personRow.map(function(personElement) {
+	return new BorderedCell(
+	    "name: " + personElement.name + "\n" + 
+		"sex: " + personElement.sex + "\n" + 
+		"born: " + personElement.born + "\n" + 
+		"died: " + personElement.died + "\n" + 
+		"father: " + personElement.father + "\n" + 
+		"mother: " + personElement.mother
+	);
+    });
+});
 
+console.log(drawTable(people));
+
+//people.push([new BorderedCell("Mastro's Ocean\nClub"), new BorderedCell("$$$$")]);
+
+//theEnd
 console.log();
