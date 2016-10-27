03af5b6ce60060cd8f7197ef1024d37216f9aab7
dannypadilla <disavowed10@gmail.com>
2016-10-27 10:31:03 -0700

    create server stuff

--- contactListApp/public/controllers/controller.js
+++ contactListApp/public/controllers/controller.js
@@ -1,24 +1,39 @@
 var myApp = angular.module("myApp", []);
+
 myApp.controller("AppCtrl", ["$scope", "$http",
 			     function($scope, $http) {
 				 console.log("Herrow wrrld");
-				 person1 = {
-				     name:"Rick",
-				     email:"rick@roll.com",
-				     number:"111-111-1111"
+				 $http.get("/contactList").sucess(function(response) {
+				     console.log("I got the data I requested");
+				     $scope.contactList = response;
+				 });
+//				 $http.get("/contactList");
+				 /*
+				  person1 = {
+				  name:"Rick",
+				  email:"rick@roll.com",
+				  number:"111-111-1111"
+				  };
+				  person2 = {
+				  name:"Roy",
+				  email:"roy@orbi.com",
+				  number:"222-222-2222"
+				  };
+				  person3 = {
+				  name:"Rhianna",
+				  email:"rhianna@ho.com",
+				  number:"333-333-3333"
+				  };
+				  */
+				 var refresh = function() {
+				     $http.get("/contactList").success(function(response) {
+					 console.log("Got Data!");
+					 $scope.contactList = response;
+				     });
 				 };
-				 person2 = {
-				     name:"Roy",
-				     email:"roy@orbi.com",
-				     number:"222-222-2222"
-				 };
-				 person3 = {
-				     name:"Rhianna",
-				     email:"rhianna@ho.com",
-				     number:"333-333-3333"
-				 };
+				 refresh();
 
-				 var contactList = [person1, person2, person3];
-				 $scope.contactList = contactList;
+//				 var contactList = [person1, person2, person3];
+//				 $scope.contactList = contactList;
 			     }]);
 
--- contactListApp/server.js
+++ contactListApp/server.js
@@ -3,7 +3,36 @@ var express = require("express");
 
 var app = express();
 
+//var mongojs = require("mongojs");
+//var db = mongojs("contactList", ["contactList"]);
+
 app.use(express.static(__dirname + "/public") );
+
+app.get("/contactList", function(request, response) {
+    console.log("I received a GET request");
+
+    person1 = {
+	name: "Rick",
+	email: "bro@bruh.br",
+	number: "111-111-1111"
+    },
+    person2 = {
+	name: "Rhianna",
+	email: "rhi@hoIsList.ho",
+	number: "222-222-2222"
+    },
+    person3 = {
+	name: "Roy O",
+	email: "Orbison@son.com",
+	number: "333-333-3333"
+    };
+
+    var contactList = [person1, person2, person3];
+    response.json(contactList);
+
+//app.post("/contactList", function(request, response) {
+
+});
 
 app.listen(3000);
 
