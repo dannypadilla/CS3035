ee98d1683b932189a098f8ca81979d15d015fbcf
dannypadilla <disavowed10@gmail.com>
2016-11-01 11:23:13 -0700

    cleaup add gitignore

--- contactListApp/server.js
+++ contactListApp/server.js
@@ -6,17 +6,14 @@ var db = mongojs("contactList", ["contactList"]);
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json() );
-
 app.use(express.static(__dirname + "/public") );
 
 app.get("/contactList", function(request, response) {
     console.log("I received a GET request");
-    
     db.contactList.find(function(err, docs) {
 	console.log(docs);
 	response.json(docs);
     });
-
 });
 
 app.post("/contactList", function(request, response) {
