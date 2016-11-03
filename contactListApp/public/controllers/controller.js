6838c0b8d2c010c67ac5a9a16b324fb19ff2de71
dannypadilla <disavowed10@gmail.com>
2016-11-03 16:00:12 -0700

    finish project, not working

--- contactListApp/public/controllers/controller.js
+++ contactListApp/public/controllers/controller.js
@@ -1,31 +1,51 @@
 
 var myApp = angular.module("myApp", []);
+// In LA!!
+myApp.controller("AppCtrl", ["$scope", "$http", function($scope, $http) {
+    console.log("Herrow wrrld");
 
-myApp.controller("AppCtrl", ["$scope", "$http",
-			     function($scope, $http) {
-				 console.log("Herrow wrrld");
+    var refresh = function() {
+	$http.get("/contactList").success(function(response) {
+	    console.log("Got Data!");
+	    $scope.contactList = response;
+	    $scope.contact = "";
+	});
+    };
+    refresh();
 
-				 var refresh = function() {
-				     $http.get("/contactList").success(function(response) {
-					 console.log("Got Data!");
-					 $scope.contactList = response;
-				     });
-				 };
-				 refresh();
+    $scope.addContact = function() {
+	console.log("entry: " + $scope.contact );
+	$http.post("/contactList", $scope.contact).success(function(response) {
+	    console.log(response);
+	    refresh();
+	});
+    };
 
-				 $scope.addContact = function() {
-				     $http.post("/contactList", $scope.contact).success(function(response) {
-					 console.log(response);
-					 refresh();
-				     });
-				 };
+    $scope.remove = function(id) {
+	console.log(id);
+	$http.delete("/contactList/" + id).success(function(response) {
+	    refresh();
+	});
+    };
 
-				 $scope.remove = function(id) {
-				     console.log(id);
-				     $http.delete("/contactList" + id).success(function(response) {
-					 refresh();
-				     });
-				 };
 
-			     }]);
+    $scope.edit = function(id) {
+	console.log(id);
+	$http.get("/contactList/" + id).success(function(response) {
+	    $scope.contact = response;
+	});
+    };
+
+    $scope.update = function(id) {
+	console.log($scope.contact._id);
+	$http.put("/contactList/" + $scope.contact._id, $scope.contact).success(function(response) {
+	    refresh();
+	});
+    };
+
+    $scope.deselect = function() {
+	$scope.contact = "";
+    };
+
+}]);
 
--- contactListApp/server.js
+++ contactListApp/server.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var app = express();
 var mongojs = require("mongojs");
@@ -6,6 +5,8 @@ var db = mongojs("contactList", ["contactList"]);
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json() );
+// In New York
+
 app.use(express.static(__dirname + "/public") );
 
 app.get("/contactList", function(request, response) {
@@ -21,6 +22,34 @@ app.post("/contactList", function(request, response) {
     db.contactList.insert(request.body, function(err, docs) {
 	response.json(docs);
     });
+});
+
+app.delete("/contactList/:id", function (request, response) {
+    var id = request.params.id;
+    console.log(id);
+    db.contactList.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+	response.json(doc);
+    });
+});
+
+app.get("/contactList/:id", function (request, response) {
+    var id = request.params.id;
+    console.log(id);
+    db.contactList.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+	response.json(doc);
+    });
+});
+
+app.put("/contactList/:id", function (request, response) {
+    var id = request.params.id;
+    console.log(id);
+    db.contactList.findAndModify (
+	{ query: { _id: mongojs.ObjectId(id) },
+	  update: { $set: {name: request.body.name, email: request.body.email, number: request.body.number} },
+	  new : true
+	}, function (err, doc) {
+	    response.json(doc);
+	});
 });
 
 app.listen(3000);
